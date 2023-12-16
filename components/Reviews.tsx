8637ae1a77e94c30ed2775de97076b4cd50a2e6f
khadjafatima409 <khadijafatima530@gmail.com>
2023-12-15 23:25:55 -0800

    completed review

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -1,45 +1,47 @@
 import Image from "next/image";
-import { FaStar } from "react-icons/fa";
+import { MdStarRate } from "react-icons/md";
 import React from "react";
 
 const Reviews = () => {
   return (
     <div className="border bg-taelish-blue">
-      <div className=" w-[80%] mx-auto grid grid-cols-3 border">
+      <div className=" w-[85%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ">
         {/* card */}
-        {/* {reviewsData.map((review) => (
-          <div
-            key={review.id}
-            className="border flex flex-col justify-center items-center text-center gap-4"
-          >
-            <Image
-              src={review.imagePath}
-              alt={review.altText}
-              height={40}
-              width={40}
-              className="rounded-full"
-            />
-            <div className="flex gap-1">
-              {[...Array(review.stars)].map((_, index) => (
-                <FaStar key={index} className="text-yellow-500" />
-              ))}
-            </div>
-            <p className="text-slate-500">{review.reviewText}</p>
-            <p className="text-indigo-500">{review.rating}</p>
-          </div>
-        ))} */}
-        {reviewsData.map((element) => {
-          return(
-
-          )
+        {reviewsData.map((review) => {
+          return (
+            <>
+              <div className="flex flex-col justify-center items-center text-center py-10 px-2 gap-4 ">
+                <Image
+                  src={review.imagePath}
+                  alt={review.altText}
+                  height={40}
+                  width={40}
+                  className="rounded-full "
+                />
+                <div className="flex">
+                  {[...Array(review.stars)].map(() => {
+                    return (
+                      <>
+                        <MdStarRate className="text-yellow-500 text-xl" />
+                      </>
+                    );
+                  })}
+                </div>
+                <div>
+                  <p className="text-slate-500 italic">{review.reviewText}</p>
+                </div>
+                <p className="text-indigo-500">{review.rating}</p>
+              </div>
+            </>
+          );
         })}
-        
       </div>
     </div>
   );
 };
 
 export default Reviews;
+
 const reviewsData = [
   {
     imagePath: "/reviews/testimonial-01.jpg",
@@ -47,6 +49,23 @@ const reviewsData = [
     stars: 5,
     reviewText:
       "Unbelievable returns! This investment has completely transformed my financial future. I've never seen anything like it.",
+    rating: "4.9/5",
+  },
+
+  {
+    imagePath: "/reviews/testimonial-02.jpg",
+    altText: "Testimonial 2",
+    stars: 5,
+    reviewText:
+      "As a seasoned investor, I can confidently say this is the best opportunity I've come across. The results speak for themselves - unparalleled growth and impressive profits.",
+    rating: "4.7/5",
+  },
+  {
+    imagePath: "/reviews/testimonial-03.jpg",
+    altText: "Testimonial 3",
+    stars: 5,
+    reviewText:
+      "I was sceptical at first, but after diving in, I can say without a doubt that this is the best investment move I've ever made. The returns are simply mind-blowing",
     rating: "4.9/5",
   },
 ];
