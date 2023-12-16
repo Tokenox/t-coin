797032ce4ae5f04223fc9d69e2cc60f035ee6f6a
khadjafatima409 <khadijafatima530@gmail.com>
2023-12-15 19:40:36 -0800

    add custom button, color

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,29 @@
 import React from "react";
+import CustomButton from "./CustomButton";
 
 const Header = () => {
   return (
     <div className="bg-taelish-blue h-screen">
-      <div className="w-[60%] border">
-        <h1>EXPLORE tHE ELITE WORLD OF T NFT</h1>
-        <p>
-          Step into the world of T NFTs, where simplicity meets sophistication.
-          Explore unique pieces with golden 'T' symbols, each telling a story of
-          digital elegance and artistic charm.
-        </p>
+      <div className="w-[50%] border text-white mx-auto flex  flex-col gap-10 py-10">
+        <div className="flex flex-col gap-5 pb-5">
+          <div className="px-10">
+            <h1 className="text-5xl font-bold text-center  font-space ">
+              EXPLORE tHE ELITE WORLD OF T NFT
+            </h1>
+          </div>
+          <p className="text-center px-5">
+            Step into the world of T NFTs, where simplicity meets
+            sophistication. Explore unique pieces with golden T symbols, each
+            telling a story of digital elegance and artistic charm.
+          </p>
+        </div>
+        <div className="flex justify-center items-center gap-8">
+          <CustomButton btnText="Connect" className="bg-blue-Violet" />
+          <CustomButton
+            btnText="LearnMore"
+            className="border border-blue-Violet"
+          />
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        space: ["Space Grotesk", "sans-serif"],
+      },
       colors: {
         "taelish-blue": "#0F172A",
+        "blue-Violet": "#710CF6",
       },
     },
   },
