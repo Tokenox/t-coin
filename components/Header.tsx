6fac8fa1fc920656d85e224181a415ba90365d97
khadjafatima409 <khadijafatima530@gmail.com>
2023-12-15 18:52:23 -0800

    add color and text in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,15 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div style={{ background: "#0F172A" }}>
-      Header
-      <h4 className="bg-white text-red-700">Khadija</h4>
+    <div className="bg-taelish-blue h-screen">
+      <div className="w-[60%] border">
+        <h1>EXPLORE tHE ELITE WORLD OF T NFT</h1>
+        <p>
+          Step into the world of T NFTs, where simplicity meets sophistication.
+          Explore unique pieces with golden 'T' symbols, each telling a story of
+          digital elegance and artistic charm.
+        </p>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "taelish-blue": "#0F172A",
+      },
+    },
   },
   plugins: [],
 };
