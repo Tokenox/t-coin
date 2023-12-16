7aa965aafbc0a0942cd42f799066ac07a8fd0009
khadjafatima409 <khadijafatima530@gmail.com>
2023-12-15 22:45:40 -0800

    define array structure

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -7,34 +7,46 @@ const Reviews = () => {
     <div className="border bg-taelish-blue">
       <div className=" w-[80%] mx-auto grid grid-cols-3 border">
         {/* card */}
-        <div className="border flex flex-col justify-center items-center text-center gap-4">
-          {/* <div className="h-10  w-10 rounded-full"> */}
-          <Image
-            src="/reviews/testimonial-01.jpg"
-            alt=""
-            height={40}
-            width={40}
-            className="rounded-full"
-          />
-          <div className="flex gap-1">
-            <FaStar className="text-yellow-500" />
-            <FaStar className="text-yellow-500" />
-            <FaStar className="text-yellow-500" />
-            <FaStar className="text-yellow-500" />
-            <FaStar className="text-yellow-500" />
+        {/* {reviewsData.map((review) => (
+          <div
+            key={review.id}
+            className="border flex flex-col justify-center items-center text-center gap-4"
+          >
+            <Image
+              src={review.imagePath}
+              alt={review.altText}
+              height={40}
+              width={40}
+              className="rounded-full"
+            />
+            <div className="flex gap-1">
+              {[...Array(review.stars)].map((_, index) => (
+                <FaStar key={index} className="text-yellow-500" />
+              ))}
+            </div>
+            <p className="text-slate-500">{review.reviewText}</p>
+            <p className="text-indigo-500">{review.rating}</p>
           </div>
-          <p className="text-slate-500">
-            &quot;Unbelievable returns! This investment has completely
-            transformed my financial future. I&apos;ve never seen anything like
-            it.&quot;
-          </p>
+        ))} */}
+        {reviewsData.map((element) => {
+          return(
 
-          <p className="text-indigo-500">4.9/5</p>
-          {/* </div> */}
-        </div>
+          )
+        })}
+        
       </div>
     </div>
   );
 };
 
 export default Reviews;
+const reviewsData = [
+  {
+    imagePath: "/reviews/testimonial-01.jpg",
+    altText: "Testimonial 1",
+    stars: 5,
+    reviewText:
+      "Unbelievable returns! This investment has completely transformed my financial future. I've never seen anything like it.",
+    rating: "4.9/5",
+  },
+];
