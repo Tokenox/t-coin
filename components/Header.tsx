40405e1f74b4fddad703d9ded3a9d133251ea11d
khadjafatima409 <khadijafatima530@gmail.com>
2023-12-15 18:31:42 -0800

    configure tailwind

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Header = () => {
-  return <div style={{ background: "#0F172A" }}>Header</div>;
+  return (
+    <div style={{ background: "#0F172A" }}>
+      Header
+      <h4 className="bg-white text-red-700">Khadija</h4>
+    </div>
+  );
 };
 
 export default Header;
